f4cc95f1f9dea08fb53c405716c2b2d116ef8073
Jurtly <jurtly@icloud.com>
2015-12-14 18:02:35 +0000

    nitrous-backup at Mon Dec 14 18:02:35 UTC 2015

---
+++ lib/greeting.rb
@@ -0,0 +1,20 @@
+def greeting
+  puts "Hello All"
+end
+
+num_of_attendees = 547
+
+puts "There are #{num_of_attendees} people coming to Jaye's birthday party."
+greeting
+
+answer = " < fill in your answer here > "
+puts "A group of flamingoes is called a #{answer}."
+
+answer = "flamboyance"
+puts "A group of flamingoes is called a #{answer}."
+
+def the_shining(name)
+  puts "Here's #{name}!"
+end
+
+puts the_shining "Johnny"

24a08eaf8551a2c4043421d6c4460b460589526f
AdamGoyer <adam@adamgoyer.com>
2016-01-14 02:00:24 -0800

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,34 @@
 # Define the variable board below.
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+# Position 1: Top Left
+board[0]
+
+# Position 2: Top Middle
+board[1]
+
+# Position 3: Top Right
+board[2]
+
+# Position 1: Middle Left
+board[3]
+
+# Position 2: Middle Middle
+board[4]
+
+# Position 3: Middle Right
+board[5]
+
+# Position 1: Bottom Left
+
+board[6]
+
+# Position 2: Bottom Middle
+board[7]
+
+# Position 3: Bottom Right
+board[8]
+
+# Move X to the Position 2, Middle
+# row[1] = "X"
+# row #=> [" ", "X", " "]

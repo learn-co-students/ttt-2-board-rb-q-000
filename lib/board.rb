177c0f78dc71a869c41d73bc7f348f821367008c
Kman6 <Ksalkey@gmail.com>
2016-01-20 16:08:24 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,2 +1,2 @@
 # Define the variable board below.
-board = ["position 1", 'position 2', "position 3", "position 4", "position 5", "position 6", "position 7", "position 8", "position 9]
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]

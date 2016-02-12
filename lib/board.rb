4cd0dd681163a7025ef76e7b71819c2f1cc8c226
cyanocobalamin <orenjuice@yahoo.com>
2016-02-12 15:21:59 -0500

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,5 +1,5 @@
 # Define the variable board below.
 board = [" ", " ", " "," ", " ", " "," ", " ", " "]
-board [5] = "O"
-board [8] = "X"
-puts board
+#board [5] = "O"
+#board [8] = "X"
+#puts board

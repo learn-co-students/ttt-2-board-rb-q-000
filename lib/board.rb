26f056a4864c14944029b8749b0ae614a2e6e306
avanover0311 <avanover0311@gmail.com>
2016-02-15 03:11:36 +0000

    nitrous-backup at Mon Feb 15 03:11:36 UTC 2016

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,10 @@
-board= [" "," "," "," "," "," "," "," "," "]# Define the variable
+board = ["X", "X", "X", "X","X", "X", "X", "X"]
+puts "#{board[0]}, #{board[1]}, #{board[2]}, #{board[3]}, #{board[4]}, #{board[5]}, #{board[6]}, #{board[7]}, #{board[8]}"
+def display_board(board)
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+display_board(board)

ccb7c82113c9c931f59c6efd3ebc54df59d66d79
Bear-on257 <imarryshow@hotmail.com>
2016-02-18 00:10:12 +0000

    nitrous-backup at Thu Feb 18 00:10:12 UTC 2016

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,12 @@
 # Define the variable board below.
+board=[""]
+board_array= { " ", " ", " ", " ", " ", " ", " ", " ", " "}
+board[0]
+baord[1]
+board [2]
+board[3]
+board[4]
+board[5]
+board][6]
+baord[7]
+board[8]

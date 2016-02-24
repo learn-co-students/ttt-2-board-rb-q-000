f5dbc113aaa10ca6e61b3f67983d410e1677e203
Cauliflowrr <ceeceebeme@gmail.com>
2016-02-24 00:19:06 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,9 +1,3 @@
-# lib/tic_tac_toe/cell.rb
-module TicTacToe
-  class Cell
-    attr_accessor :value
-    def initialize(value = "")
-      @value = value
-    end
-  end
-end
+
+  board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+

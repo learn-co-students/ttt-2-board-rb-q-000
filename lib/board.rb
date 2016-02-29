7f5c8016881dd4096e9c21cd80c2b04dc675670e
Bear-on257 <imarryshow@hotmail.com>
2016-02-29 12:27:05 +0000

    nitrous-backup at Mon Feb 29 12:27:05 UTC 2016

--- lib/board.rb
+++ lib/board.rb
@@ -1,12 +1,15 @@
-# Define the variable board below.
-board=[""]
-board_array= { " ", " ", " ", " ", " ", " ", " ", " ", " "}
-board[0]
-baord[1]
-board [2]
+ lib/board.rb=board
+board = [ " ", " ", " ", " ", " ", " ", " ", " " ," " ]
+board_array= [ " " ]
+board[0]=[""]
+baord[1]=[""]
+board[2]
 board[3]
 board[4]
 board[5]
-board][6]
-baord[7]
+board[6]
+board[7]
 board[8]
+board.each do[d]
+ puts d
+end

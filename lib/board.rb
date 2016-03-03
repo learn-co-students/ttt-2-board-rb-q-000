06a1e0a54be3435220a9064ecb2b1188fc7915f9
joesurfa <joesurfa130@yahoo.com>
2016-03-03 14:55:52 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,15 +1,2 @@
 # Define the variable board below.
-row = [" "," "]
-row = [" "," "]
-row = [" "," "]
-
-row[0]
-row[1]
-row[2]
-row[3]
-row[4]
-row[5]
-row[6]
-row[7]
-row[8]
-row[5] = "x"
+row = [" "," "," "," "," "," "," "," "," "]

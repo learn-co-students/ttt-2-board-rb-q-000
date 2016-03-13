42929dbda12975d8170bd5822002744907058b71
drewvollrath <drew@vollrath.ca>
2016-03-13 06:35:20 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,3 +1,11 @@
 # Define the variable board below.
 
 board = [" "," "," "," "," "," "," "," "," "]
+
+def display_board
+  puts "  |   |   "
+  puts "-----------"
+  puts "  |   |   "
+  puts "-----------"
+  puts "  |   |   "
+end

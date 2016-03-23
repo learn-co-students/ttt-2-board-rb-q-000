d59e7e4159b8d338d07c744466bba737e344692c
Marylprier <priertroyandmary@gmail.com>
2016-03-23 21:18:36 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,10 @@
-# Define the variable board below.
+#define an array board
+#board will be a variable that represents an array
+# #{board[0]}  |   |
+#-----------
+#   |   |
+#-----------
+#   |   |
+#board will have 9 elements in the array
+# the elements can contain empty values, ""
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]

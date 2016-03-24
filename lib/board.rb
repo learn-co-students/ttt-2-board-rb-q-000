eeb0c359573743e111716682c96778de1af0d69e
Scott <swakeman@ca.rr.com>
2016-03-24 04:44:43 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -5,18 +5,3 @@ row [0]
 row [1]
 # position 3 : right
 row [2]
-row = [" ", ' ', ' ' ]
-# position 4 : left
-row [3]
-# position 5 : middle
-row [4]
-# position 6 : right
-row [5]
-# position 7 : left
-row =[ " ", " ", " " ]
-row [6]
-# position 8 : middle
-row [7]
-# position 9 : right
-row [8]
-Define the variable board below.

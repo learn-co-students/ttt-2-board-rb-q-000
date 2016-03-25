a4720eeb4ffb068dd8e236d932981ad3e80d1beb
Scott <swakeman@ca.rr.com>
2016-03-25 23:16:25 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,18 +1,18 @@
-row = [" ", " " , " "]
+row = [" ", " ", " "]
 # position 1 : left
 row [0]
 # position 2 : middle
 row [1]
 # position 3 : right
 row [2]
-row = [" ", " " , " "]
+row = [" ", " ", " "]
 # position 1 : left
 row [3]
 # position 1 : middle
 row [4]
 # position 1 : right
 row [5]
-row = [" ", " " , " "]
+row = [" ", " ", " "]
 # position 1 : left
 row [6]
 # position 1 : middle

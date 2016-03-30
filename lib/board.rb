971ad0eb09749e7ff0fc88827af3bc1ccdb8f4ca
dietrich13 <dietrich13@gmail.com>
2016-03-30 01:29:26 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,15 @@
-# Define the variable board below.
+board = [" "," "," "," "," "," "," "," "," "]
+
+#top left
+board[0]
+board[1]
+board[2]
+#second row left
+board[3]
+board[4]
+board[5]
+board[6]
+board[7]
+#bottom right
+board[8]
+

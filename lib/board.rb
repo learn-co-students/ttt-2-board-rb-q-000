acb112d7a4d84fc18cbd74b8f956e18fab84199d
blazks <bedalje@gmail.com>
2016-04-02 10:00:20 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,5 +1,2 @@
 # Define the variable board below.
-row1=[" ","|"," ","|"," "]
-row2=[" ","|"," ","|"," "]
-row3=[" ","|"," ","|"," "]
-put row1 row2 row2
+board = [" "," "," "," "," "," "," "," "," "]

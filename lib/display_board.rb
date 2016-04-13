13c722692f310ca9b80e5bfae8256d45bb4e8d35
naughtyperson <will.mowat@gmail.com>
2016-04-13 13:30:09 +0000

    Done.

---
+++ lib/display_board.rb
@@ -0,0 +1,12 @@
+  def display_board
+  puts "Welcome to Tic Tac Toe"
+  cell="   "
+  div1="-----------"
+  div2="|"
+  puts "#{cell}#{div2}#{cell}#{div2}#{cell}"
+  puts div1
+  puts "#{cell}#{div2}#{cell}#{div2}#{cell}"
+  puts div1
+  puts "#{cell}#{div2}#{cell}#{div2}#{cell}"
+  end
+puts display_board

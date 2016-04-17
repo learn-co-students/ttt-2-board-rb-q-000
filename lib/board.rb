36a9ab5203c440c096fdcd912dc2ad7ac0716afe
shktkt219 <shktkt219@gmail.com>
2016-04-17 03:33:06 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,36 @@
 # Define the variable board below.
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+row = [" ", " ", " "]
+
+# position 1: Left
+row[0]
+
+# position 2: Middle
+row[1]
+
+# position 3: Right
+row[2]
+
+row =[" ", " ", " "]
+
+# position 4: Left
+row[3]
+
+# position 5: middle
+row[4]
+
+# position 6: Right
+row[5]
+
+row =[" ", " ", " "]
+
+# position 7: Left
+row[6]
+
+# position 8: Middle
+row[7]
+
+# position 9: Right
+row[8]
+

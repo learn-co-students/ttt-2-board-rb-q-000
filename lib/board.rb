eccd57afbe4841e6a62e9181804d0b8618c63bd1
Terri Lake <tlake25@gmail.com>
2016-04-24 13:58:45 +0000

    nitrous-backup at Sun Apr 24 13:58:45 UTC 2016

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,31 @@
 # Define the variable board below.
+row1 = [" ", " ", " "]
+
+row2 = [" ", " ", " "]
+
+row3 = [" ", " ", " "]
+
+# Position 1: Left
+row1[0]
+
+# Position 2: Middle
+row1[1]
+
+# Position 3: Right
+row1[2]
+
+row2[3]
+
+row2[4]
+
+row2[5]
+
+row3[6]
+
+row3[7]
+
+row3[8]
+
+# Move X to the Position 2, Middle
+row[1] = "X"
+row #=> [" ", "X", " "]

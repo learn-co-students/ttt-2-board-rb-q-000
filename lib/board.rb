68b159c0a8e128203aca7e2f799b3ea7992f8326
Bear-on257 <Bear-on257@users.noreply.github.com>
2016-05-30 00:53:46 -0400

    Update board.rb

--- lib/board.rb
+++ lib/board.rb
@@ -1,15 +1,24 @@
  lib/board.rb=board
 board = [ " ", " ", " ", " ", " ", " ", " ", " " ," " ]
 board_array= [ " " ]
+#position1 row 1 left
 board[0]=[""]
+#position2 row 1 middle
 baord[1]=[""]
-board[2]
-board[3]
-board[4]
-board[5]
-board[6]
-board[7]
-board[8]
+#Position 3 row 1 right
+board[2]=[" "]
+#Position 4 row 2 left
+board[3]=[" "]
+#position 5  row 2 middle
+board[4]= [" "]
+#position6 row 2 right
+board[5]=[" "]
+#position7 row 3 left
+board[6]={" "]
+#Position8 row 3 middle
+board[7]= [" "]
+#position9 row 3 right
+board[8]=[" "]
 board.each do[d]
  puts d
 end
